18d618fa0ba05f031422f91338d5bf06253be247
Kushal Raj Sharma <kuxall0@gmail.com>
2023-05-09 22:06:05 +0545

    Update ner app

---
+++ scripts/app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import spacy
+from spacy import displacy
+
+# Load the model
+nlp = spacy.load(r"/home/kushal/Documents/ner_app/model/2023-02-24_model/2023-02-24_test_model/model-best/")
+
+# Define the Streamlit app
+def main():
+    st.title("Named Entity Recognition Demo")
+    st.write("Enter some text below to extract named entities:")
+    text = st.text_area("Input text:", height=200)
+    if st.button("Extract Entities"):
+        doc = nlp(text)
+        if doc.ents:
+            html = displacy.render(doc, style="ent")
+            st.write(html, unsafe_allow_html=True)
+        else:
+            st.write("No entities found in the input text.")
+
+if __name__ == "__main__":
+    main()
